783a156302093af2df35fb2edbc256436b7cb4f0
Thabiso Klip <thebzklip0@gmail.com>
2020-07-31 11:21:03 +0200

    Updates

---
+++ precache-manifest.9bc1affc09cc6fa41e300cc3107cd7eb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb4cad3362ee39a8a5f3e0a8942371b",
+    "url": "/estratwenimobilefoods/index.html"
+  },
+  {
+    "revision": "018e739fcf21e8c64138",
+    "url": "/estratwenimobilefoods/static/css/main.66dead97.chunk.css"
+  },
+  {
+    "revision": "6d391cc5d3b06b22bbb5",
+    "url": "/estratwenimobilefoods/static/js/2.4918c400.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/estratwenimobilefoods/static/js/2.4918c400.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "018e739fcf21e8c64138",
+    "url": "/estratwenimobilefoods/static/js/main.884d5318.chunk.js"
+  },
+  {
+    "revision": "8e4943a9098cbc42bd0c",
+    "url": "/estratwenimobilefoods/static/js/runtime-main.f7bf39ef.js"
+  },
+  {
+    "revision": "396608a4d6e30243a64a7b938584ae2c",
+    "url": "/estratwenimobilefoods/static/media/background_4.396608a4.jpg"
+  },
+  {
+    "revision": "a871241862efa9a8e1035cf0662fa615",
+    "url": "/estratwenimobilefoods/static/media/itembackground.a8712418.jpg"
+  },
+  {
+    "revision": "9688c03314002d75149f37054ddc1def",
+    "url": "/estratwenimobilefoods/static/media/itembackground_2.9688c033.jpg"
+  },
+  {
+    "revision": "d823241570fae92dbf97081c3849ba2f",
+    "url": "/estratwenimobilefoods/static/media/logo.d8232415.jpg"
+  }
+]);
