fc102b4c8581e3ba4bd5beb08aeabc63fdb3a2bb
Thabiso Klip <thebzklip0@gmail.com>
2021-05-12 09:57:19 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 // import React from "react";
 // import "./App.scss";
-// import { Login, Register } from "./components/login/index";
 
 // class App extends React.Component {
 //   constructor(props) {
@@ -29,27 +28,9 @@
 //   }
 
 //   render() {
-//     const { isLogginActive } = this.state;
-//     const current = isLogginActive ? "Register" : "Login";
-//     const currentActive = isLogginActive ? "login" : "register";
 //     return (
 // 			<div className="App">
-// 				<div className="login">
-// 					<div className="container" ref={(ref) => (this.container = ref)}>
-// 						{isLogginActive && (
-// 							<Login containerRef={(ref) => (this.current = ref)} />
-// 						)}
-// 						{!isLogginActive && (
-// 							<Register containerRef={(ref) => (this.current = ref)} />
-// 						)}
-// 					</div>
-// 					<RightSide
-// 						current={current}
-// 						currentActive={currentActive}
-// 						containerRef={(ref) => (this.rightSide = ref)}
-// 						onClick={this.changeState.bind(this)}
-// 					/>
-// 				</div>
+// 				
 // 				<Fragment>
 // 					<Navbar />
 // 					<About />
